d439e429936480687b604376458d8ab64cf19336
callumzhong <callum.zhong@gmail.com>
2022-06-17 19:49:11 +0800

    feat: 新增 rpgui-content class 樣式

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ const App = () => {
 		// 	<Route path='/village' element={<VillagePage />} /> */}
 		// </Routes>
 		<div className='mx-5 mt-5'>
-			<Container mode='framed-golden'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-				laboriosam adipisci explicabo itaque corporis eligendi incidunt quia est
-				atque aperiam, obcaecati ipsam deserunt quaerat, quos sit id numquam
-				doloremque fuga? Deserunt consectetur possimus accusamus consequuntur
-				amet quis fugit nostrum maiores ullam illo magni accusantium inventore
-				labore eius obcaecati ut assumenda exercitationem id hic cum nulla
-				voluptatibus, facilis in? Odit, culpa. Minus, adipisci officiis
-				laudantium aperiam quo temporibus nihil reprehenderit magnam tempore,
-				assumenda est quod explicabo autem cumque rerum corporis, ea ducimus
-				beatae sapiente. Omnis nisi eaque cum? Architecto, ipsum debitis!
-			</Container>
+			<div className='rpgui-content'>
+				<Container mode='framed-golden'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+					laboriosam adipisci explicabo itaque corporis eligendi incidunt quia
+					est atque aperiam, obcaecati ipsam deserunt quaerat, quos sit id
+					numquam doloremque fuga? Deserunt consectetur possimus accusamus
+					consequuntur amet quis fugit nostrum maiores ullam illo magni
+					accusantium inventore labore eius obcaecati ut assumenda
+					exercitationem id hic cum nulla voluptatibus, facilis in? Odit, culpa.
+					Minus, adipisci officiis laudantium aperiam quo temporibus nihil
+					reprehenderit magnam tempore, assumenda est quod explicabo autem
+					cumque rerum corporis, ea ducimus beatae sapiente. Omnis nisi eaque
+					cum? Architecto, ipsum debitis!
+				</Container>
+			</div>
 		</div>
 	);
 };
