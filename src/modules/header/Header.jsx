76465097cd5e638ff00574cc22b8222ac843cc11
callumzhong <callum.zhong@gmail.com>
2022-06-28 06:12:05 +0800

    feat: add header ui component

--- src/modules/header/Header.jsx
+++ src/modules/header/Header.jsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import styles from './Bookmark.module.css';
-const Bookmark = ({ mode, className, children }) => {
+import styles from './Header.module.css';
+
+const Header = ({ mode, className, children }) => {
   return (
-    <div
+    <header
       className={clsx(
         'inline-block leading-none rendering-pixelated',
         styles.border,
@@ -12,13 +13,13 @@ const Bookmark = ({ mode, className, children }) => {
       )}
     >
       {children}
-    </div>
+    </header>
   );
 };
 
-Bookmark.propTypes = {
+Header.propTypes = {
   className: PropTypes.string,
   children: PropTypes.any,
 };
 
-export default Bookmark;
+export default Header;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,14 +1,17 @@
 import FoldArrow from 'modules/arrow/FoldArrow';
 import Bookmark from 'modules/bookmark/Bookmark';
+import Button from 'modules/button/Button';
+import Header from 'modules/header/Header';
 
 const SignInPage = (props) => {
   return (
-    <div className='flex flex-col items-center pt-5 pl-5'>
+    <div className='flex flex-col items-center gap-2 pt-5 pl-5'>
+      <Button>12</Button>
       <Bookmark>12</Bookmark>
-      <Bookmark className='mt-5' mode={'action'}>
-        12
-      </Bookmark>
+      <Bookmark mode={'action'}>12</Bookmark>
       <FoldArrow>設置</FoldArrow>
+      <Header>角色資訊大平台</Header>
+      <Header mode='reverse'>角色資訊大平台</Header>
     </div>
     // <Layout>
 
